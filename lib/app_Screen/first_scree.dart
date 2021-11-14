769ae13f237d86b3853ed2015bbe281c8551100c
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-14 11:25:11 -0800

    flutter app

---
+++ lib/app_Screen/first_scree.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class firstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  const Material(
+      color: Colors.black12,
+      child: Center(
+        child: Text('hello flutter'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
-import 'package:flutter/cupertino.dart';
-import "package:flutter/material.dart";
-//librery which contain flutter widget
-void main(){
-  runApp(
-   const Center(
-     child: Text("hello flutter",
-              textDirection:TextDirection.ltr,
-            style: TextStyle(color: Colors.red,fontSize: 20,fontWeight: FontWeight.bold),
-        ),
-   )
-  );//runapp attach widget to the screen
-  
+import 'package:flutter/material.dart';
+
+import 'app_Screen/first_scree.dart';
+
+void main()=>runApp(myflutterapp());
+
+class myflutterapp extends StatelessWidget {
+//  const myflutterapp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: "flutter app",
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Myapp",
+              style: TextStyle(
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.5,
+              ),
+            ),
+          ),
+          body: firstScreen();
+        ));
+  }
 }
